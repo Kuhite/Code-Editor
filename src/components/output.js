d8b52bf74f4f695882a493a569e7a0b99dd4c6cb
Aayush Amar Kuhite <aayush.kuhite2@gmail.com>
2021-12-12 17:15:58 +0530

    Working Completed

--- src/components/output.js
+++ src/components/output.js
@@ -1,42 +1,69 @@
 import {useRef, useState} from 'react';
-import $ from 'jquery';
+
 
 const Output = () => {
-    const htmlContainer = useRef("");
-    const cssContainer = useRef("");
-    const jsContainer = useRef("");
-  
-    const DisplayOutput = () => {
-        const HTML = htmlContainer.current.value;
-        const CSS = cssContainer.current.value;
-        const JS = jsContainer.current.value;
-        $("iframe").contents().find("html").html(
-            "<html><head><style type = 'text/css'>"+CSS+"</style></head><body>"+HTML+"<script type='text/javascript'></script></body></html>");
-            document.getElementById("iframe").contentWindow.eval($("js").val());
+    const htmlContainer = useRef(null);
+    const cssContainer = useRef(null);
+    const jsContainer = useRef(null);
+ 
+
+
+    const DisplayOutput = (e) => {
+        e.preventDefault();
+        var HTML =htmlContainer.current.value;
+        var CSS = cssContainer.current.value;
+        var JS = jsContainer.current.value;
+
+        if(HTML == null){
+            HTML = "";
+        }
+        if(CSS == null){
+            CSS = "";
+        }
+        if(JS
+             == null){
+           JS = "";
+        }
+
+
+        var output = "<html><head><style type = 'text/css'>"+CSS+"</style></head><body>"+HTML+
+        "</body></html>"
+        console.log(JS);
+        document.getElementById("iframe").contentWindow.eval(JS);
+        document.getElementById("iframe").contentWindow.document.body.innerHTML = output ;
+
+        
     }
     
 
     return(
         <div>
             <div className ="input">
-                <div id = "html" className="input">
-                    <button>HTML</button>
+                <form onSubmit={DisplayOutput}>
+                <button type="submit" className="outputButton">Output</button>
+                <div id = "html" >
+                    <button>HTML</button><br/>
                     <textarea ref={htmlContainer}></textarea>
                 </div>
-                <div id = "css" className="input">
-                    <button>CSS</button>
+                <div id = "css" >
+                    <button>CSS</button><br/>
                     <textarea ref={cssContainer}></textarea>
                 </div>
-                <div id = "js" className="input">
-                    <button>JS</button>
+                <div id = "js">
+                    <button>JS</button><br/>
                     <textarea ref={jsContainer}></textarea>
                 </div>
+                <div >
+               
+                </div>
+                </form>
             </div>
 
             <div className = "output">
-                <button onClick={DisplayOutput}>Output</button>
-                <iframe id ="iframe" title="Output"></iframe>
-            </div>
+                <h1>Output</h1>
+                <a><iframe  id ="iframe" title="Output"></iframe></a>
+                <div id="FinalOutput"></div>
+                </div>
 
         </div>
     )
